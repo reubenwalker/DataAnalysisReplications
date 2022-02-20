b57026a5d6faaf2734dd70ee98425a4d28e45334
reubenwalker <reuben.scott.walker@gmail.com>
2022-02-20 16:38:06 +0100

    cute messages

--- DeckDrawNumpy.py
+++ DeckDrawNumpy.py
@@ -1,22 +1,16 @@
-#Mechanic to choose decks from a pot
 import random
-n = int(input('How many decks are there?'))
-potOrder = list(range(1,(n+1)))
-#pO = potOrder
-cont = input('Press enter to pull first deck or enter "n" to quit.')
-while ((potOrder != []) & (cont.lower() != 'n')):
-    deck = str(pO.pop(random.randint(0,len(pO)-1)))
-    cont = input('Deck number: ' + deck
-                 +', Pull next deck? Press enter or enter "n" to quit.')
-    print(deck)
-
+import numpy as np
 #Numpy alternative
 #Mechanic to choose decks from a pot
-n = int(input('How many decks are there?'))
-potOrder = list(np.random.permutation(range(1,(n+1))))
-pO = potOrder
+numDecks = int(input('How many decks are there?'))
+potOrder = list(np.random.permutation(range(1,(numDecks+1))))
 cont = ''
-while ((pO != []) & (cont.lower() != 'n')):
-    cont = input('Print next deck? Press enter or enter "n" to quit.')
-    print('Deck number: ' + str(pO.pop()))
-
+while ((potOrder != []) & (cont.lower() != 'q')):
+    cont = input('Print next deck? Press enter or enter "q" to quit.\n')
+    if(cont.lower() == 'q'):
+        print('Have fun!')
+        break
+    print('Deck number: ' + str(potOrder.pop()))
+    if(potOrder == []):
+        print("That's all the decks!")
+    
