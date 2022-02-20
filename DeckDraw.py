a879355c3aa866cbfc014bd4b5712c14a0b446cd
reubenwalker <reuben.scott.walker@gmail.com>
2022-02-20 15:24:25 +0100

    Renamed variables, commenting

--- DeckDraw.py
+++ DeckDraw.py
@@ -1,12 +1,11 @@
 #Mechanic to choose decks from a pot
 import random
-n = int(input('How many decks are there?'))
-potOrder = list(range(1,(n+1)))
-pO = potOrder
+numDecks = int(input('How many decks are there?'))
+potOrder = list(range(1,(numDecks+1)))
+#pO = potOrder
 cont = input('Press enter to pull first deck or enter "n" to quit.')
-while ((pO != []) & (cont.lower() != 'n')):
-    deck = str(pO.pop(random.randint(0,len(pO)-1)))
+while ((potOrder != []) & (cont.lower() != 'n')):
+    deck = str(potOrder.pop(random.randint(0,len(potOrder)-1)))
     cont = input('Deck number: ' + deck
                  +', Pull next deck? Press enter or enter "n" to quit.')
-    print(deck)
 
